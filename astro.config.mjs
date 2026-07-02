87d1af4050064a6b95b71bb029b4ef4b51314ed2
eliascvrno <eliascvrno@gmail>
2026-07-02 16:00:20 -0500

    feat: create base layout component with SEO support and update site URL in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jez-7.github.io',
+  site: 'https://eliascvrno.vercel.app',
   integrations: [tailwind()]
 });
